a5dc6945a5921db110ee6216593d92ca7ff8ec28
Wildan Arya <wildanarya707@gmail.com>
2024-11-10 10:32:45 +0700

    select role feature

--- resources/js/Pages/Auth/Register.jsx
+++ resources/js/Pages/Auth/Register.jsx
@@ -1,30 +1,48 @@
-import InputError from '@/Components/InputError';
-import InputLabel from '@/Components/InputLabel';
-import PrimaryButton from '@/Components/PrimaryButton';
-import TextInput from '@/Components/TextInput';
-import GuestLayout from '@/Layouts/GuestLayout';
-import { Head, Link, useForm } from '@inertiajs/react';
+import React, { useEffect } from "react";
+import InputError from "@/Components/InputError";
+import InputLabel from "@/Components/InputLabel";
+import PrimaryButton from "@/Components/PrimaryButton";
+import TextInput from "@/Components/TextInput";
+import GuestLayout from "@/Layouts/GuestLayout";
+import { Head, Link, useForm } from "@inertiajs/react";
 
 export default function Register() {
     const { data, setData, post, processing, errors, reset } = useForm({
-        name: '',
-        email: '',
-        password: '',
-        password_confirmation: '',
+        name: "",
+        email: "",
+        number: "",
+        address: "",
+        role: "",
+        password: "",
+        password_confirmation: "",
     });
+
+    useEffect(() => {
+        const queryParams = new URLSearchParams(window.location.search);
+        const roleParam = queryParams.get("role");
+        if (roleParam) {
+            setData("role", roleParam); // Set role ke dalam form data
+        }
+    }, []);
 
     const submit = (e) => {
         e.preventDefault();
 
-        post(route('register'), {
-            onFinish: () => reset('password', 'password_confirmation'),
+        post(route("register"), {
+            onFinish: () => reset("password", "password_confirmation"),
         });
     };
 
     return (
         <GuestLayout>
             <Head title="Register" />
-
+            <div className="text-center">
+                {data.role === "fotografer" ? (
+                    <h1>Sign Up as a Photographer</h1>
+                ) : (
+                    <h1>Sign Up to Find Your Photographer</h1>
+                )}
+            </div>
             <form onSubmit={submit}>
                 <div>
                     <InputLabel htmlFor="name" value="Name" />
@@ -36,7 +54,7 @@ export default function Register() {
                         className="mt-1 block w-full"
                         autoComplete="name"
                         isFocused={true}
-                        onChange={(e) => setData('name', e.target.value)}
+                        onChange={(e) => setData("name", e.target.value)}
                         required
                     />
 
@@ -53,11 +71,47 @@ export default function Register() {
                         value={data.email}
                         className="mt-1 block w-full"
                         autoComplete="username"
-                        onChange={(e) => setData('email', e.target.value)}
+                        onChange={(e) => setData("email", e.target.value)}
                         required
                     />
 
                     <InputError message={errors.email} className="mt-2" />
+                </div>
+
+                <div className="mt-4">
+                    <InputLabel
+                        htmlFor="number"
+                        value="Phone Number"
+                    ></InputLabel>
+
+                    <TextInput
+                        id="number"
+                        type="number"
+                        name="number"
+                        value={data.number}
+                        className="mt-1 block w-full"
+                        autoComplete="new-number"
+                        onChange={(e) => setData("number", e.target.value)}
+                        required
+                    />
+
+                    <InputError message={errors.number} className="mt-2" />
+                </div>
+
+                <div className="mt-4">
+                    <InputLabel htmlFor="address" value="Address"></InputLabel>
+
+                    <TextInput
+                        id="address"
+                        name="address"
+                        value={data.adress}
+                        className="mt-1 block w-full"
+                        autoComplete="new-adress"
+                        onChange={(e) => setData("adress", e.target.value)}
+                        required
+                    />
+
+                    <InputError message={errors.address} className="mt-2" />
                 </div>
 
                 <div className="mt-4">
@@ -70,7 +124,7 @@ export default function Register() {
                         value={data.password}
                         className="mt-1 block w-full"
                         autoComplete="new-password"
-                        onChange={(e) => setData('password', e.target.value)}
+                        onChange={(e) => setData("password", e.target.value)}
                         required
                     />
 
@@ -91,7 +145,7 @@ export default function Register() {
                         className="mt-1 block w-full"
                         autoComplete="new-password"
                         onChange={(e) =>
-                            setData('password_confirmation', e.target.value)
+                            setData("password_confirmation", e.target.value)
                         }
                         required
                     />
@@ -104,7 +158,7 @@ export default function Register() {
 
                 <div className="mt-4 flex items-center justify-end">
                     <Link
-                        href={route('login')}
+                        href={route("login")}
                         className="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                     >
                         Already registered?
