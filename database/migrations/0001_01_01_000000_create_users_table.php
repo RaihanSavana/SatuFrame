cc98684fc1093ae540546cb80ae36605b9bb7dd3
Raihan Savana <raihansavana727@gmail.com>
2024-11-10 16:26:23 +0700

    nomor telepon  become string

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->integer('nomor_telepon')->unique();
+            $table->string('nomor_telepon')->unique();
             $table->string('alamat');
             $table->enum('role', ['user', 'fotografer'])->default('user');
             $table->timestamp('email_verified_at')->nullable();
